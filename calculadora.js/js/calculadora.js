8942809c46fe16ec678054e9d59eb720c52b1673
dahianacalderon <140189369+dahianacalderon@users.noreply.github.com>
2023-10-02 10:29:14 -0500

    njjj
    njjn

--- calculadora.js/js/calculadora.js
+++ calculadora.js/js/calculadora.js
@@ -1,97 +1,111 @@
-opc=parseInt(prompt("Ingresa la opcion que quieras elegir 1. Area Cuadrado  2. Area Rectangulo 3. Area Triangulo 4. Suma 5. Resta. 6. Multiplicacion 7. Division 8. Cual es mayor 9. Salir "));
-    switch(opc){
-        case 1:
-            lado= parseInt(prompt("Ingresa La base"));
-            var resultado = lado*lado;
-            alert ("El area es " +resultado);
-                if(base != 0){
-                    document.write("El resultado del area del cuadrado es :" + resultado);
-                }else{
-                    alert("Error, ingrese valores diferentes de cero");
-                }
-            break;
-        case 2:
-            base= parseInt(prompt("Ingresa La base"));
-            altura = parseInt(prompt("Ingresa La Altura"));
-            var resultado = base*altura;
-            alert ("El area es " +resultado);
-                if(base != 0 && altura != 0){
-                    document.write("El resultado del area del rectangulo es :" + resultado);
-                }else{
-                    alert("Error, ingrese valores diferentes de cero");
-                }
-            break;
-        case 3:
-            base= parseInt(prompt("Ingresa La base"));
-            altura = parseInt(prompt("Ingresa La Altura"));
-            var resultado = base*altura/2;
-            alert ("El area es " +resultado);
-                if(base != 0 && altura != 0){
-                    document.write("El resultado del area del triangulo es :" + resultado);
-                }else{
-                    alert("Error, ingrese valores diferentes de cero");
-                }
-            break;
-        case 4:
-            suma= parseInt(prompt("Ingresa el primer numero a sumar"));
-            suma= parseInt(prompt("Ingresa el segundo numero a sumar"));
-            var resultado= suma+suma;
-            alert ("La suma es "+resultado) ;
-                if(suma != 0 && suma != 0){
-                    document.write ("El resultado de la suma es : " + resultado);
-                }else{
-                    alert("Error, ingrese valores diferentes de cero");
-                }
-        case 5:
-            resta= parseInt(prompt("Ingresa el primer numero a restar"));
-            resta= parseInt(prompt("Ingresa el segundo numero a restar"));
-            var resultado= resta-resta;
-            alert ("La resta es "+resultado) ;
-                if(suma != 0 && suma != 0){
-                    document.write ("El resultado de la resta es: " + resultado);
-                }else{
-                    alert("Error, ingrese valores diferentes de cero");
-                }
-        case 6:
-            multiplicacion= parseInt(prompt("Ingresa el primer numero a multiplicar "));
-            multiplicacion= parseInt(prompt("Ingresa el segundo numero a multiplicar "));
-            var resultado= multiplicacion*multiplicacion;
-            alert ("La multplicacion es "+resultado) ;
-                if(suma != 0 && suma != 0){
-                    document.write ("El resultado de la multiplicacion es : " + resultado);
-                }else{
-                    alert("Error, ingrese valores diferentes de cero");
-                }
-        case 7:
-            division= parseFloat(prompt("Ingresa el primer numero a dividir "));
-            division2= parseFloat(prompt("Ingresa el segundo numero a dividir "));
-            var resultado= division/division2;
-            alert ("La division es "+resultado) ;
-                if(suma != 0 && suma != 0){
-                    document.write ("El resultado de la division es : " + resultado);
-                }else{
-                    alert("Error, ingrese valores diferentes de cero");
-                }
-        case 8:
-            numero1 = parseFloat(prompt("Ingresa el primer numero :"));
-            numero2= parseFloat(prompt("Ingresa el segundo numero :"))
-            var resultado= numero1 / numero2;
-            if (numero1 > numero2) {
-                document.write("El número " + numero1 + " es mayor que " + numero2);
-            } else if (numero2 > numero1) {
-                document.write("El número " + numero2 + " es mayor que " + numero1);
-            }else{
-                alert("Error, ingrese valores diferentes de cero");
-            }
-        case 9:
-            alert("Gracias por usar la calculadora vuelva pronto");
-            break;
-        default:
-            alert("Opcion no valida");
-    }
+// Solicitar al usuario que ingrese una opción
+var opc = parseInt(prompt("Ingresa la opción que quieras elegir:\n1. Area Cuadrado\n2. Area Rectangulo\n3. Area Triangulo\n4. Suma\n5. Resta\n6. Multiplicacion\n7. Division\n8. Cual es mayor\n9. Salir"));
 
-
-
+// Utilizar un switch para manejar diferentes casos según la opción ingresada
+switch (opc) {
+    case 1:
+        // Calcular el área de un cuadrado
+        var lado = parseInt(prompt("Ingresa la longitud del lado del cuadrado"));
+        var resultado = lado * lado;
+        alert("El área del cuadrado es: " + resultado);
+        if (lado != 0) {
+            document.write("El área del cuadrado es: " + resultado);
+        } else {
+            alert("Error, ingrese un valor diferente de cero");
+        }
+        break;
+    case 2:
+        // Calcular el área de un rectángulo
+        var base = parseInt(prompt("Ingresa la longitud de la base del rectángulo"));
+        var altura = parseInt(prompt("Ingresa la altura del rectángulo"));
+        var resultado = base * altura;
+        alert("El área del rectángulo es: " + resultado);
+        if (base != 0 && altura != 0) {
+            document.write("El área del rectángulo es: " + resultado);
+        } else {
+            alert("Error, ingrese valores diferentes de cero");
+        }
+        break;
+    case 3:
+        // Calcular el área de un triángulo
+        var base = parseInt(prompt("Ingresa la longitud de la base del triángulo"));
+        var altura = parseInt(prompt("Ingresa la altura del triángulo"));
+        var resultado = (base * altura) / 2;
+        alert("El área del triángulo es: " + resultado);
+        if (base != 0 && altura != 0) {
+            document.write("El área del triángulo es: " + resultado);
+        } else {
+            alert("Error, ingrese valores diferentes de cero");
+        }
+        break;
+    case 4:
+        // Realizar una suma
+        var suma1 = parseFloat(prompt("Ingresa el primer número a sumar"));
+        var suma2 = parseFloat(prompt("Ingresa el segundo número a sumar"));
+        var resultado = suma1 + suma2;
+        alert("La suma es: " + resultado);
+        if (suma1 != 0 && suma2 != 0) {
+            document.write("El resultado de la suma es: " + resultado);
+        } else {
+            alert("Error, ingrese valores diferentes de cero");
+        }
+        break;
+    case 5:
+        // Realizar una resta
+        var resta1 = parseFloat(prompt("Ingresa el primer número a restar"));
+        var resta2 = parseFloat(prompt("Ingresa el segundo número a restar"));
+        var resultado = resta1 - resta2;
+        alert("La resta es: " + resultado);
+        if (resta1 != 0 && resta2 != 0) {
+            document.write("El resultado de la resta es: " + resultado);
+        } else {
+            alert("Error, ingrese valores diferentes de cero");
+        }
+        break;
+    case 6:
+        // Realizar una multiplicación
+        var multiplicacion1 = parseFloat(prompt("Ingresa el primer número a multiplicar"));
+        var multiplicacion2 = parseFloat(prompt("Ingresa el segundo número a multiplicar"));
+        var resultado = multiplicacion1 * multiplicacion2;
+        alert("La multiplicación es: " + resultado);
+        if (multiplicacion1 != 0 && multiplicacion2 != 0) {
+            document.write("El resultado de la multiplicación es: " + resultado);
+        } else {
+            alert("Error, ingrese valores diferentes de cero");
+        }
+        break;
+    case 7:
+        // Realizar una división
+        var division1 = parseFloat(prompt("Ingresa el dividendo"));
+        var division2 = parseFloat(prompt("Ingresa el divisor"));
+        var resultado = division1 / division2;
+        alert("La división es: " + resultado);
+        if (division2 != 0) {
+            document.write("El resultado de la división es: " + resultado);
+        } else {
+            alert("Error, el divisor no puede ser cero");
+        }
+        break;
+    case 8:
+        // Comparar cuál de dos números es mayor
+        var numero1 = parseFloat(prompt("Ingresa el primer número"));
+        var numero2 = parseFloat(prompt("Ingresa el segundo número"));
+        if (numero1 > numero2) {
+            document.write("El número " + numero1 + " es mayor que " + numero2);
+        } else if (numero2 > numero1) {
+            document.write("El número " + numero2 + " es mayor que " + numero1);
+        } else {
+            alert("Los números son iguales");
+        }
+        break;
+    case 9:
+        // Salir del programa
+        alert("Gracias por usar la calculadora. ¡Vuelve pronto!");
+        break;
+    default:
+        // Opción no válida
+        alert("Opción no válida");
+}
 
 
 
